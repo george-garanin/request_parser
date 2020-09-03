790a5b315ab40cf775d3d149a75a8bbef8a0aa46
georgega <ggaranin@ceki.ru>
2020-09-03 18:41:55 +0300

    format output

--- parser.rb
+++ parser.rb
@@ -12,3 +12,11 @@ prepared_data = Loaders::LogFile.new(ARGV[0]).load
 
 visits = Computes::AllVisits.new(prepared_data).calculate
 uniq_views = Computes::UniqueVisits.new(parsed_file).calculate
+
+pp "List of webpages with most page views ordered from most pages views to less page views"
+visits.each { |visit| pp "#{visit[0]} #{visit[1]} visits" }
+
+pp "------"
+
+pp "list of webpages with most unique page views ordered from most unique page views to less"
+uniq_views.each { |views| pp "#{views[0]} #{views[1]} unique views" }
